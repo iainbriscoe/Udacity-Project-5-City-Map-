62233143576dc6c68bf7250fd6c82a34afc432e4
Iain Briscoe <iainbriscoe@Iains-MacBook-Pro-2.local>
2015-11-10 03:17:49 -0500

    map marker work

--- js/app.js
+++ js/app.js
@@ -10,56 +10,56 @@ function viewModel(){
 		lat : 43.4753,
 		lng : -80.5272,
 		link : "http://en.wikipedia.org/w/api.php?action=opensearch&search=wilfrid_laurier_university&format=json&callback=wikiCallback",
-		valueString : "tt"	
+		valueString : "1"	
 	},
 	{ 
 		name : "University of Waterloo",
 		lat : 43.4689,
 		lng : -80.5400,
 		link : "http://en.wikipedia.org/w/api.php?action=opensearch&search=University_of_waterloo&format=json&callback=wikiCallback",
-		valueString : "tt"
+		valueString : "2"
 	},
 	{ 
 		name : "Conestoga College",
 		lat : 43.479239,
 		lng : -80.517953,
 		link : "http://en.wikipedia.org/w/api.php?action=opensearch&search=Conestoga_college&format=json&callback=wikiCallback",
-		valueString : "tt"
+		valueString : "3"
 	}, 
 	{
 		name : "Phils",
 		lat : 43.476095,
 		lng : -80.524554,
 		link : "http://en.wikipedia.org/w/api.php?action=opensearch&search=Phil_Gaglardi&format=json&callback=wikiCallback",
-		valueString : "tt"
+		valueString : "4"
 	}, 
 	{
 		name : "Chainsaw",
 		lat : 43.466123,
 		lng :  -80.522361,
 		link : "http://en.wikipedia.org/w/api.php?action=opensearch&search=waterloo,_ontario&format=json&callback=wikiCallback",
-		valueString : "tt"
+		valueString : "5"
 	},
 	{ 
 		name : "Waterloo Park",
 		lat : 43.466490,
 		lng : -80.532416,
 		link : "http://en.wikipedia.org/w/api.php?action=opensearch&search=waterloo,_ontario&format=json&callback=wikiCallback",
-		valueString : "tt"
+		valueString : "6"
 	}, 
 	{
 		name : "Bechtel Park",
 		lat : 43.482695,
 		lng :  -80.491708,
 		link : "http://en.wikipedia.org/w/api.php?action=opensearch&search=waterloo,_ontario&format=json&callback=wikiCallback",
-		valueString : "tt"
+		valueString : "7"
 	},
 	{ 
 		name : "RIM Park",
 		lat : 43.519484,
 		lng : -80.502236,
 		link : "http://en.wikipedia.org/w/api.php?action=opensearch&search=rim_park&format=json&callback=wikiCallback",
-		valueString : "tt"
+		valueString : "8"
 	}];
 
 	self.Places = ko.observableArray(dataArray);
@@ -115,20 +115,22 @@ function viewModel(){
         		
         		var infowindow = new google.maps.InfoWindow({
 					content: item.valueString
-					});
-					//marker.infowindow.close();
-					infowindow.open(viewModel.map.googleMap, marker);
+				});
+				
+				//marker.infowindow.close();
+				infowindow.open(viewModel.map.googleMap, this);
 
-					openedInfoWindow = infowindow; 
+				viewModel.openedInfoWindow = infowindow; 
 
-					google.maps.event.addListener(infowindow, 'closeclick', function() {
-					openedInfoWindow = null;
-					});
-					
-					google.maps.event.addListener(viewModel.map.googleMap, 'click', function() {
-						console.log("test"); 
-						infoWindow.close();
-					});
+				google.maps.event.addListener(infowindow, 'closeclick', function() {
+					viewModel.openedInfoWindow = null;
+				});
+				/*
+				google.maps.event.addListener(viewModel.map.googleMap, "click", function() {
+					console.log("test"); 
+					infowindow.close();
+				});
+				*/
 
 		    });
 			marker.setMap(viewModel.map);
